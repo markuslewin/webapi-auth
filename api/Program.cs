96378c0a796546b67b019341d696ba53d62764fb
Markus Lewin <lewinmarkus@gmail.com>
2026-05-21 23:16:11 +0200

    Set up ef+postgres

--- api/Program.cs
+++ api/Program.cs
@@ -1,11 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+builder.AddNpgsqlDbContext<BloggingContext>(connectionName: "postgresdb");
+// builder.Services.AddDbContextPool<BloggingContext>(opt =>
+//     opt.UseNpgsql(builder.Configuration.GetConnectionString("BloggingContext")));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,29 +24,68 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", async (BloggingContext ctx) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var blog = await ctx.Blogs.FindAsync(1);
+    return blog.Url;
+
+    // var forecast = Enumerable.Range(1, 5).Select(index =>
+    //     new WeatherForecast
+    //     (
+    //         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+    //         Random.Shared.Next(-20, 55),
+    //         summaries[Random.Shared.Next(summaries.Length)]
+    //     ))
+    //     .ToArray();
+    // return forecast;
 })
 .WithName("GetWeatherForecast");
 
-app
-    .MapGroup("/account")
-    .MapIdentityApi<User>();
+// app
+//     .MapGroup("/account")
+//     .MapIdentityApi<User>();
+
+using (var scope = app.Services.CreateScope())
+{
+    var ctx = scope.ServiceProvider.GetRequiredService<BloggingContext>();
+    ctx.Database.EnsureCreated();
+    ctx.Blogs.Add(new Blog
+    {
+        BlogId = 1,
+        Url = "hey"
+    });
+    ctx.SaveChanges();
+}
 
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class BloggingContext(DbContextOptions<BloggingContext> options) : DbContext(options)
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
 }
 
 class User
--- apphost.cs
+++ apphost.cs
@@ -1,7 +1,14 @@
+﻿#:package Aspire.Hosting.PostgreSQL@13.3.5
 #:sdk Aspire.AppHost.Sdk@13.3.3+a4615e7c6def6cba4703cdbd84009cd3da9a261b
+#:project ./api/webapi-auth.csproj
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-// The aspireify skill will wire up your projects here.
+var postgres = builder.AddPostgres("postgres");
+var postgresdb = postgres
+  .AddDatabase("postgresdb");
+
+var exampleProject = builder.AddProject<Projects.webapi_auth>("apiservice")
+  .WithReference(postgresdb);
 
 builder.Build().Run();
